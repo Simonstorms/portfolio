67ab9f88bfc00a65088eda04a239725f0d64a498
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-11 00:06:47 +0100

    2/2 projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import {IoCloseOutline} from "react-icons/io5";
 import Image from 'next/image';
 import {ImArrowUpRight2} from "react-icons/im";
+import {MdOutlineExpandMore} from "react-icons/md";
 
 
 
@@ -14,54 +15,175 @@ export default function Home() {
                 <Link className="flex items-center mb-5 justify-center" href="http://localhost:3000/">
                     <div className="border  p-3  border-black rounded-full "><IoCloseOutline size={20}/></div>
                 </Link>
-                <div>
+                <div className="flex justify-center p-8 px-12  font-bold text-5xl">Projects</div>
+
+                <div className="">
 
 
                     <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
                         <input type="radio" name="my-accordion-3"/>
-                        <div className="collapse-title text-xl pe-4 font-medium">
-                            <div className="relative h-64 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
-                                <div className="absolute inset-0 rounded-[2rem] transform -rotate-12 -translate-y-6 translate-x-6">
+                        <div className="collapse-title text-xl  p-5 font-medium">
+                            <div
+                                className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
+                                <div className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
                                     {/* New container div for the image */}
-                                    <div className="relative h-full rounded-[2rem] w-full scale-75">
-                                    <Image src={"/images/innomed.png"} layout={"fill"} objectFit={"cover"}
-                                           alt={"Angled box image"}/>
+                                    <div className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
+                                        <Image src={"/images/innomed.png"} layout={"fill"} objectFit={"cover"}
+                                               alt={"Angled box image"}/>
                                     </div>
                                 </div>
 
                                 <button className="absolute bottom-4 left-4 p-2 bg-white rounded-full shadow-lg">
-                                    <Link href="https://innomedsaxony.de">
-                                    <ImArrowUpRight2/>
-                                    </Link>
+                                    <MdOutlineExpandMore/>
                                 </button>
 
                             </div>
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <div className="">
+                                <div className="text-2xl mb-5">Innomed Saxony</div>
+                                <div className="mb-2">The "InnoMed Saxony" project aims to network players in the fields
+                                    of medicine and business in Saxony and to accelerate medical technology transfer.
+                                </div>
+                                <ul className="mb-2">
+                                    <li>- Supports from basic research to market launch</li>
+                                    <li>- Assistance with funding applications and recruitment</li>
+                                    <li>- hosted events: networking, moderated talks, workshops</li>
+                                </ul>
+                            </div>
+
+
+                            <div className=" rounded-full w-24 mx-3 border-4 ">
+                                <Link
+                                    href="https://innomedsaxony.de"
+
+                                >
+                                    <div
+                                        className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full bg-white px-4 py-1 text-text">
+
+                                        <span className="">Web</span>
+                                        <div className=""><ImArrowUpRight2/></div>
+
+                                    </div>
+                                </Link>
+                            </div>
+
                         </div>
                     </div>
-                    <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white collapse-plus ">
-                        <input type="radio" name="my-accordion-3" />
-                        <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+
+
+                        <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
+                            <input type="radio" name="my-accordion-3"/>
+                            <div className="collapse-title text-xl pe-4 font-medium">
+                                <div
+                                    className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
+                                    <div
+                                        className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
+                                        {/* New container div for the image */}
+                                        <div
+                                            className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
+                                            <Image src={"/images/aidm.png"} layout={"fill"} objectFit={"cover"}
+                                                   alt={"Angled box image"}/>
+                                        </div>
+                                    </div>
+
+                                    <button className="absolute bottom-4 left-4 p-2 bg-white rounded-full shadow-lg">
+                                        <MdOutlineExpandMore/>
+                                    </button>
+
+                                </div>
+                            </div>
+                            <div className="collapse-content">
+                                <div className="">
+                                    <div className="text-2xl mb-5">Innomed Saxony</div>
+                                    <div className="mb-2">The "Innomed Saxony" project aims to network players in the
+                                        fields of medicine and business in Saxony and to accelerate medical technology
+                                        transfer.
+                                    </div>
+                                    <ul className="mb-2">
+                                        <li>- Supports from basic research to market launch</li>
+                                        <li>- Assistance with funding applications and recruitment</li>
+                                        <li>- hosted events: networking, moderated talks, workshops</li>
+                                    </ul>
+                                </div>
+
+
+                                <div className=" rounded-full w-24 mx-3 border-4 ">
+                                    <Link
+                                        href="https://innomedsaxony.de"
+
+                                    >
+                                        <div
+                                            className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full bg-white px-4 py-1 text-text">
+
+                                            <span className="">Web</span>
+                                            <div className=""><ImArrowUpRight2/></div>
+
+                                        </div>
+                                    </Link>
+                                </div>
+
+                            </div>
                         </div>
-                        <div className="collapse-content">
-                            <p>hello</p>
+
+
+
+                            <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
+                                <input type="radio" name="my-accordion-3"/>
+                                <div className="collapse-title text-xl pe-4 font-medium">
+                                    <div
+                                        className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
+                                        <div
+                                            className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
+                                            {/* New container div for the image */}
+                                            <div
+                                                className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
+                                                <Image src={"/images/question.jpeg"} layout={"fill"} objectFit={"cover"}
+                                                       alt={"Angled box image"}/>
+                                            </div>
+                                        </div>
+
+                                        <button
+                                            className="absolute bottom-4 left-4 p-2 bg-white rounded-full shadow-lg">
+                                            <MdOutlineExpandMore/>
+                                        </button>
+
+                                    </div>
+                                </div>
+                                <div className="collapse-content">
+                                    <div className="">
+                                        <div className="text-2xl mb-5">Innomed Saxony</div>
+                                        <div className="mb-2">The "InnoMed Saxony" project aims to network players in
+                                            the fields of medicine and business in Saxony and to accelerate medical
+                                            technology transfer.
+                                        </div>
+                                        <ul className="mb-2">
+                                            <li>- Supports from basic research to market launch</li>
+                                            <li>- Assistance with funding applications and recruitment</li>
+                                            <li>- hosted events: networking, moderated talks, workshops</li>
+                                        </ul>
+                                    </div>
+
+
+                                    <div className=" rounded-full w-24 mx-3 border-4 ">
+                                        <Link
+                                            href="https://innomedsaxony.de"
+
+                                        >
+                                            <div
+                                                className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full bg-white px-4 py-1 text-text">
+
+                                                <span className="">Web</span>
+                                                <div className=""><ImArrowUpRight2/></div>
+
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            <Footer/>
                         </div>
-                    </div>
-                    <div className="collapse  rounded-[2rem]  dark:bg-[#0a0d0f]  bg-white collapse-plus ">
-                        <input type="radio" name="my-accordion-3" />
-                        <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
-                        </div>
-                        <div className="collapse-content">
-                            <p>hello</p>
-                        </div>
-                    </div>
-                </div>
-                <Footer/>
-            </div>
         </main>
-    )
+)
 }
