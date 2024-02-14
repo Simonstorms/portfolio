8ea133b4df8d0bd650a512ec208d7d500e49ce4b
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-14 16:45:18 +0100

    Update main-part.tsx

--- components/main-part.tsx
+++ components/main-part.tsx
@@ -24,41 +24,41 @@ return (
 
     <div className="grid  [grid-auto-columns:285px] [grid-auto-rows:132.5px]  gap-5 sm:grid-cols-2  xl:grid-cols-4 grid-cols-1
    ">
-        {/*<div className="shadow-xl col-span-1 sm:col-span-2  row-span-3 sm:row-span-2 dark:bg-[#0a0d0f] rounded-[2rem] bg-white">*/}
-        {/*    <div className=""><About /></div>*/}
-        {/*</div>*/}
-{/*
+        <div className="shadow-xl col-span-1 sm:col-span-2  row-span-3 sm:row-span-2 dark:bg-[#0a0d0f] rounded-[2rem] bg-white">
+            <div className=""><About /></div>
+        </div>
+
         <div className=" col-span-1 row-span-4 rounded-[2rem]   dark:bg-[#0a0d0f]  bg-white"><Projects/></div>
-*/}
 
-        {/*<div className="overflow-hidden rounded-[2rem] bg-map  dark:bg-[#0a0d0f]  col-span-1 row-span-2">*/}
 
-        {/*    <Suspense fallback={<div>Loading...</div>}>*/}
-        {/*        <Mapbox coords={cords}/>*/}
-        {/*    </Suspense>*/}
-        {/*</div>*/}
-        {/*<div className="col-span-1   xl:col-span-2 row-span-1 rounded-[2rem]  dark:bg-[#0a0d0f] bg-white">
-            <SocialBox/></div>*/}
-{/*
+        <div className="overflow-hidden rounded-[2rem] bg-map  dark:bg-[#0a0d0f]  col-span-1 row-span-2">
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Mapbox coords={cords}/>
+         </Suspense>
+        </div>
+        <div className="col-span-1   xl:col-span-2 row-span-1 rounded-[2rem]  dark:bg-[#0a0d0f] bg-white">
+            <SocialBox/></div>
+
         <div className="  col-span-1 row-span-4 rounded-[2rem] xl:block hidden dark:bg-[#0a0d0f]  bg-white"><Experience/></div>
-*/}
 
-        {/*<div className=" col-span-1 xl:col-span-2 row-span-1 rounded-[2rem]  dark:bg-[#0a0d0f]  bg-white"><Interest/>
-        </div>*/}
-       {/* <div className="  col-span-1 row-span-4 xl:hidden  rounded-[2rem]  dark:bg-[#0a0d0f]  bg-white"><Experience/>
-        </div>*/}
+
+        <div className=" col-span-1 xl:col-span-2 row-span-1 rounded-[2rem]  dark:bg-[#0a0d0f]  bg-white"><Interest/>
+        </div>
+       <div className="  col-span-1 row-span-4 xl:hidden  rounded-[2rem]  dark:bg-[#0a0d0f]  bg-white"><Experience/>
+        </div>
 
         <div className="  col-span-1 row-span-2 rounded-[2rem]   dark:bg-[#0a0d0f] bg-white"><Spotify/></div>
 
-        {/*<div className=" col-span-1 rounded-[2rem] xl:col-span-2 row-span-2  dark:bg-[#0a0d0f]  bg-white"><Cta/>*/}
-        {/*</div>*/}
-        {/*<div className=" col-span-1 rounded-[2rem] sm:col-span-2 sm:row-span-2 row-span-4 dark:bg-[#0a0d0f]  bg-white"><Skills/></div>*/}
+        <div className=" col-span-1 rounded-[2rem] xl:col-span-2 row-span-2  dark:bg-[#0a0d0f]  bg-white"><Cta/>
+        </div>
+        <div className=" col-span-1 rounded-[2rem] sm:col-span-2 sm:row-span-2 row-span-4 dark:bg-[#0a0d0f]  bg-white"><Skills/></div>
 
-        {/*<div className=" xl:blog items-center hidden rounded-[2rem] col-span-1 row-span-1  dark:bg-[#0a0d0f]  bg-white"><Imprint/>*/}
-        {/*</div>*/}
-        {/*<div className=" rounded-[2rem] col-span-1 row-span-2  dark:bg-[#0a0d0f]  bg-white"><ThemeToggle/></div>*/}
-        {/*<div className=" blogx items-center  xl:hidden rounded-[2rem] col-span-1 row-span-1  dark:bg-[#0a0d0f]  bg-white"><Imprint/></div>*/}
-        {/*<div className=" rounded-[2rem]  col-span-1 row-span-1  dark:bg-[#0a0d0f]  bg-white"><Policy/></div>*/}
+        <div className=" xl:block items-center hidden rounded-[2rem] col-span-1 row-span-1  dark:bg-[#0a0d0f]  bg-white"><Imprint/>
+        </div>
+        <div className=" rounded-[2rem] col-span-1 row-span-2  dark:bg-[#0a0d0f]  bg-white"><ThemeToggle/></div>
+        <div className=" block items-center xl:hidden rounded-[2rem] col-span-1 row-span-1  dark:bg-[#0a0d0f]  bg-white"><Imprint/></div>
+        <div className=" rounded-[2rem]  col-span-1 row-span-1  dark:bg-[#0a0d0f]  bg-white"><Policy/></div>
 
 
     </div>
