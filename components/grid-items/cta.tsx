11ed490b2c59667db21c5210b34903e97e02b96c
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-22 11:42:11 +0100

    email

--- components/grid-items/cta.tsx
+++ components/grid-items/cta.tsx
@@ -17,7 +17,7 @@ const Cta = () => {
                     <div
                         className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
                     </div>
-                    <Link href="mailto:test" title="Contact me"
+                    <Link href="mailto:info@simongneuss.com" title="Contact me"
                        className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-800 dark:bg-gray-950 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                        role="button">Contact me
                     </Link>
