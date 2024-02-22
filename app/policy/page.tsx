8e92ae1221401ac5b99be96231f5622bc21f82ae
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-22 11:17:57 +0100

    Update page.tsx

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <main className="max-w-6xl mx-auto p-10 ">
             <div className="px-8 w-full">
-                <Link className="flex items-center justify-center" href="http://localhost:3000/">
+                <Link className="flex items-center justify-center" href="/">
                     <div className="border  p-3  border-black rounded-full "><IoCloseOutline size={20}/></div>
                 </Link>
                 Policy
