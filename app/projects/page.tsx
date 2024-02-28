6397bf27dfc875ec9e9093e01b3418fe2111ce6b
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-28 14:18:37 +0100

    project fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -27,168 +27,180 @@ export default function Home() {
 
                 <div className="">
 
+                    <label onClick="">
+                        <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
 
-                    <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
-                        <input type="radio" name="my-accordion-3"/>
-                        <div className="collapse-title text-xl  p-5 font-medium">
-                            <div
-                                className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
-                                <div className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
-                                    {/* New container div for the image */}
-                                    <div className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
-                                        <Image src={"/images/innomed.png"} layout={"fill"} objectFit={"cover"}
-                                               alt={"Angled box image"}/>
-                                    </div>
-                                </div>
-
-                                <button
-                                    className="absolute bottom-4 left-4 p-2 bg-white dark:bg-black rounded-full shadow-lg">
-                                    <MdOutlineExpandMore/>
-                                </button>
-
-                            </div>
-                        </div>
-                        <div className="collapse-content">
-                            <div className="">
-                                <div className="text-2xl mb-5">Innomed Saxony</div>
-                                <div className="mb-2">The &quot;InnoMed Saxony&quot; project aims to network players in
-                                    the fields
-                                    of medicine and business in Saxony and to accelerate medical technology transfer.
-                                </div>
-                                <ul className="mb-2 list-disc list-inside">
-                                    <li>Supports from basic research to market launch</li>
-                                    <li>Assistance with funding applications and recruitment</li>
-                                    <li>hosted events: networking, moderated talks, workshops</li>
-                                </ul>
-                            </div>
-
-
-                            <div className=" rounded-full dark:border-[#31353b] w-24 mx-3 border-4 ">
-                                <Link href="https://innomedsaxony.de">
-                                    <div
-                                        className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full bg-white  dark:bg-[#0a0d0f] px-4 py-1 text-text">
-
-                                        <span className="">Web</span>
-                                        <div className=""><ImArrowUpRight2/></div>
-
-                                    </div>
-                                </Link>
-                            </div>
-
-                        </div>
-                    </div>
-
-
-                    <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
-                        <input type="radio" name="my-accordion-3"/>
-                        <div className="collapse-title text-xl pe-4 font-medium">
-                            <div
-                                className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
+                            <input type="radio" name="my-accordion-3"/>
+                            <div className="collapse-title text-xl  p-5 font-medium">
                                 <div
-                                    className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
-                                    {/* New container div for the image */}
+                                    className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
                                     <div
-                                        className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
-                                        <Image src={"/images/aidm.png"} layout={"fill"} objectFit={"cover"}
-                                               alt={"Angled box image"}/>
+                                        className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
+                                        {/* New container div for the image */}
+                                        <div
+                                            className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
+                                            <Image src={"/images/innomed.png"} layout={"fill"} objectFit={"cover"}
+                                                   alt={"Angled box image"}/>
+                                        </div>
                                     </div>
+
+                                    <button
+                                        className="absolute bottom-4 left-4 p-2 bg-white dark:bg-black rounded-full shadow-lg">
+                                        <MdOutlineExpandMore/>
+                                    </button>
+
+                                </div>
+                            </div>
+
+                            <div className="collapse-content">
+                                <div className="">
+                                    <div className="text-2xl mb-5">Innomed Saxony</div>
+                                    <div className="mb-2">The &quot;InnoMed Saxony&quot; project aims to network players
+                                        in
+                                        the fields
+                                        of medicine and business in Saxony and to accelerate medical technology
+                                        transfer.
+                                    </div>
+                                    <ul className="mb-2 list-disc list-inside">
+                                        <li>Supports from basic research to market launch</li>
+                                        <li>Assistance with funding applications and recruitment</li>
+                                        <li>hosted events: networking, moderated talks, workshops</li>
+                                    </ul>
                                 </div>
 
-                                <button
-                                    className="absolute bottom-4 left-4 p-2 bg-white dark:bg-black rounded-full shadow-lg">
-                                    <MdOutlineExpandMore/>
-                                </button>
+
+                                <div className=" rounded-full dark:border-[#31353b] w-24 mx-3 border-4 ">
+                                    <Link href="https://innomedsaxony.de">
+                                        <div
+                                            className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full bg-white  dark:bg-[#0a0d0f] px-4 py-1 text-text">
+
+                                            <span className="">Web</span>
+                                            <div className=""><ImArrowUpRight2/></div>
+
+                                        </div>
+                                    </Link>
+                                </div>
 
                             </div>
                         </div>
-                        <div className="collapse-content">
-                            <div className="">
-                                <div className="text-2xl mb-5">Ab in die Mitte</div>
-                                <div className="mb-2">The Ab in die Mitte! Die City-Offensive Sachsen initiative,
-                                    launched in 2004 in Saxony, Germany, encourages vibrant urban life through over 500
-                                    projects from 140+ cities and communities. It focuses on:
-                                </div>
-                                <ul className="mb-2  list-disc list-inside">
-                                    <li>Balancing non-investment and investment activities to revitalize city centers.
-                                    </li>
-                                    <li>Enhancing multifunctionality and encouraging public participation in development
-                                        strategies.
-                                    </li>
-                                    <li>Utilizing public-private partnerships to foster innovative, sustainable urban
-                                        development and regional architectural identity.
-                                    </li>
-
-                                </ul>
-
-                            </div>
-
-
-                            <div className=" rounded-full dark:border-[#31353b]  w-24 mx-3 border-4 ">
-                                <Link
-                                    href="https://www.abindiemitte-sachsen.de/"
-
-                                >
-                                    <div
-                                        className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full dark:bg-[#0a0d0f] bg-white px-4 py-1 text-text">
-
-                                        <span className="">Web</span>
-                                        <div className=""><ImArrowUpRight2/></div>
-
-                                    </div>
-                                </Link>
-                            </div>
-
-                        </div>
-                    </div>
-
-
-                    <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
-                        <input type="radio" name="my-accordion-3"/>
-                        <div className="collapse-title text-xl pe-4 font-medium">
-                            <div
-                                className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
+                    </label>
+                    <label onClick="">
+                        <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
+                            <input type="radio" name="my-accordion-3"/>
+                            <div className="collapse-title text-xl pe-4 font-medium">
                                 <div
-                                    className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
-                                    {/* New container div for the image */}
+                                    className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
                                     <div
-                                        className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
-                                        <Image src={"/images/question.jpeg"} layout={"fill"} objectFit={"cover"}
-                                               alt={"Angled box image"}/>
+                                        className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
+                                        {/* New container div for the image */}
+                                        <div
+                                            className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
+                                            <Image src={"/images/aidm.png"} layout={"fill"} objectFit={"cover"}
+                                                   alt={"Angled box image"}/>
+                                        </div>
                                     </div>
+
+                                    <button
+                                        className="absolute bottom-4 left-4 p-2 bg-white dark:bg-black rounded-full shadow-lg">
+                                        <MdOutlineExpandMore/>
+                                    </button>
+
+                                </div>
+                            </div>
+                            <div className="collapse-content">
+                                <div className="">
+                                    <div className="text-2xl mb-5">Ab in die Mitte</div>
+                                    <div className="mb-2">The Ab in die Mitte! Die City-Offensive Sachsen initiative,
+                                        launched in 2004 in Saxony, Germany, encourages vibrant urban life through over
+                                        500
+                                        projects from 140+ cities and communities. It focuses on:
+                                    </div>
+                                    <ul className="mb-2  list-disc list-inside">
+                                        <li>Balancing non-investment and investment activities to revitalize city
+                                            centers.
+                                        </li>
+                                        <li>Enhancing multifunctionality and encouraging public participation in
+                                            development
+                                            strategies.
+                                        </li>
+                                        <li>Utilizing public-private partnerships to foster innovative, sustainable
+                                            urban
+                                            development and regional architectural identity.
+                                        </li>
+
+                                    </ul>
+
                                 </div>
 
-                                <button
-                                    className="absolute bottom-4 left-4 p-2 bg-white dark:bg-black rounded-full shadow-lg">
-                                    <MdOutlineExpandMore/>
-                                </button>
+
+                                <div className=" rounded-full dark:border-[#31353b]  w-24 mx-3 border-4 ">
+                                    <Link
+                                        href="https://www.abindiemitte-sachsen.de/"
+
+                                    >
+                                        <div
+                                            className="  gap-2 whitespace-nowrap flex items-center box-border rounded-full dark:bg-[#0a0d0f] bg-white px-4 py-1 text-text">
+
+                                            <span className="">Web</span>
+                                            <div className=""><ImArrowUpRight2/></div>
+
+                                        </div>
+                                    </Link>
+                                </div>
 
                             </div>
                         </div>
-                        <div className="collapse-content">
-                            <div className="">
-                                <div className="text-2xl mb-5">Questiontime</div>
-                                <div className="mb-2"></div>
-                                <ul className="mb-2">
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                </ul>
-                            </div>
+                    </label>
+                    <label onClick="">
 
-
-                            <div className=" rounded-full w-24 mx-3 dark:border-[#31353b]  border-4 ">
-                                <Link href="https://question-time.co.uk">
+                        <div className="collapse rounded-[2rem] mb-4 dark:bg-[#0a0d0f]  bg-white  ">
+                            <input type="radio" name="my-accordion-3"/>
+                            <div className="collapse-title text-xl pe-4 font-medium">
+                                <div
+                                    className="relative h-48 w-full bg-gradient-to-br from-yellow-400  to-pink-300 rounded-xl overflow-hidden">
                                     <div
-                                        className="  gap-2 whitespace-nowrap flex items-center box-border dark:bg-[#0a0d0f] rounded-full bg-white px-4 py-1 text-text">
-
-                                        <span className="">Web</span>
-                                        <div className=""><ImArrowUpRight2/></div>
-
+                                        className="absolute inset-0  transform -rotate-12 -translate-y-6 translate-x-6">
+                                        {/* New container div for the image */}
+                                        <div
+                                            className="relative h-full  overflow-hidden rounded-[1rem] w-full scale-75">
+                                            <Image src={"/images/question.jpeg"} layout={"fill"} objectFit={"cover"}
+                                                   alt={"Angled box image"}/>
+                                        </div>
                                     </div>
-                                </Link>
+
+                                    <button
+                                        className="absolute bottom-4 left-4 p-2 bg-white dark:bg-black rounded-full shadow-lg">
+                                        <MdOutlineExpandMore/>
+                                    </button>
+
+                                </div>
+                            </div>
+                            <div className="collapse-content">
+                                <div className="">
+                                    <div className="text-2xl mb-5">Questiontime</div>
+                                    <div className="mb-2"></div>
+                                    <ul className="mb-2">
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </div>
+
+
+                                <div className=" rounded-full w-24 mx-3 dark:border-[#31353b]  border-4 ">
+                                    <Link href="https://question-time.co.uk">
+                                        <div
+                                            className="  gap-2 whitespace-nowrap flex items-center box-border dark:bg-[#0a0d0f] rounded-full bg-white px-4 py-1 text-text">
+
+                                            <span className="">Web</span>
+                                            <div className=""><ImArrowUpRight2/></div>
+
+                                        </div>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </label>
                 </div>
                 <Footer/>
             </div>
