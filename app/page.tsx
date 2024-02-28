2910713559bcafb9f2b964e7df9b0d4a2f1cf502
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-28 15:07:52 +0100

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import MainPart from "@/components/main-part";
 import Footer from "@/components/footer";
+import Image from "next/image";
 
 
 export default function Home() {
--- components/grid-items/about.tsx
+++ components/grid-items/about.tsx
@@ -4,15 +4,21 @@ import Image from "next/image";
 
 const About = () => {
     return (
-           <div className="p-8">
-                <div className="mb-4"><Image src={GridItems["me"].image || ""} width={100} height={100} alt="Profile picture of me"/></div>
-                {/* text */}
-                <p className="leading-8">
-                    Hello 👋, I&apos;m
-                    <span className="font-bold mx-2 text-2xl">Simon</span>
-                    a Freelance Software Developer from Berlin. I am currently studying Software Engineering at the CODE University of Applied Sciences in Berlin.
-                </p>
-            </div>
+        <div className="p-8">
+            <div className="mb-4"><Image src={GridItems["me"].image || ""} width={100} height={100}
+                                         alt="Profile picture of me"/></div>
+            {/* text */}
+            <p className="leading-8">
+                Hello 👋, I&apos;m
+                {/*<span className="font-bold mx-2 text-2xl">Simon</span>*/}
+                <span className="mx-2" style={{display: "inline-block"}}>
+                    <Image src={"/images/logo.png"} alt={"Logo which says Simon"} width={80} height={24}/>
+  </span>
+                a Freelance Software Developer from Berlin. I am currently studying Software Engineering at the CODE
+                University of Applied Sciences in Berlin.
+            </p>
+
+        </div>
 
     );
 };
