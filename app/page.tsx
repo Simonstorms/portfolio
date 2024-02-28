47b317f7a258479ce753813a2bc64026608d824e
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-02-28 15:10:59 +0100

    more padding top

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
     return (
 
-    <main className="mx-auto  w-full max-w-[301px] px-2 pb-6 pt-4 sm:max-w-[606px] xl:max-w-[1200px] xl:px-0 ">
+    <main className="mx-auto  w-full max-w-[301px] px-2 pb-6 pt-10 sm:max-w-[606px] xl:max-w-[1200px] xl:px-0 ">
         <MainPart />
         <div className=" w-full">
             <Footer />
