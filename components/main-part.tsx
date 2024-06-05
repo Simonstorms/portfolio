908213731d6ccf9260d28209ce9d49631aabb3a9
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2024-06-05 08:45:58 +0200

    delete animation  Please enter the commit message for your changes. Lines starting

--- components/main-part.tsx
+++ components/main-part.tsx
@@ -11,14 +11,14 @@ import Imprint from "@/components/grid-items/imprint";
 import Policy from "@/components/grid-items/policy";
 import Cta from "@/components/grid-items/cta";
 import Experience from "@/components/grid-items/experience/experience";
-import Animated from "@/layouts/Animation-main";
+
 const cords = {
   latitude: Number(process.env.NEXT_PUBLIC_MAP_LATITUDE),
   longitude: Number(process.env.NEXT_PUBLIC_MAP_LONGITUDE),
 };
 const MainPart = () => {
   return (
-    <Animated>
+
       <div>
         <div className="grid [grid-auto-rows:132.5px] gap-5 sm:grid-cols-2  xl:grid-cols-4 grid-cols-1">
           <div className="shadow-xl col-span-1 sm:col-span-2  row-span-3 sm:row-span-2 dark:bg-[#0a0d0f] rounded-[2rem] bg-white">
@@ -68,7 +68,6 @@ const MainPart = () => {
           </div>
         </div>
       </div>
-    </Animated>
   );
 };
 
